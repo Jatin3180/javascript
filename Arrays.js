be39fe05e988d8a3b19d75866f6ca1296f1a179f
Jatin3180 <jatinyadav31801@gmail.com>
2024-07-29 11:10:36 +0530

    code updated

--- Arrays.js
+++ Arrays.js
@@ -1,9 +1,65 @@
 // Arrays
 // let arr = [ value1 , value2 , value3 , value4]
-let num = [ 1 , 2 , 3 , 4 ];
-//          0   1   2   3  
-console.log(num)
-num[2] = 15;
+// let num = [ 1 , 2 , 3 , 4 ];
+// //          0   1   2   3  
+// console.log(num)
+// num[2] = 15;
 // override
-console.log(num.length)
+// console.log(num.length)    //
 
+// ui designing 
+// api integration
+// let field_error = "/$[a-z]{1-9}^/";
+
+// 9 X 1 = 9
+// 9 x 2 = 18
+// 
+
+// Largest Number 
+// let arr = [10 , 8 , 14, 15 , 9];
+// let num = arr.at(2);
+//  str = arr.join(" * ");
+// let large = arr[0];
+// for(let i = 0 ;i<arr.length;i++){
+//       if(arr[i]>large){
+//          large = arr[i];
+//       }
+// }
+// console.log(large)
+
+// i = 0 : arr[0] > 10 X
+// i = 1 : arr[1] > 10 x
+// i = 2 : arr[2] > 10 yes
+// i = 3 : arr[3] > 14
+
+// 
+
+// let str = "batsal"
+// let str1 = ""
+// length
+// for(let i = str.length;i>=0;i--){
+//    str1  += str[i];
+// }
+// if(str1==str){
+
+// }
+// swap 
+// 345
+// 12
+
+// i = 5 ; str1  = l
+// i = 4; str1 = e
+// 
+// toString()
+// at()
+// join()
+// pop() -
+// let arr = [2, 45, 25 , 6, 45]
+// arr.indexOf(25)
+// arr.pop()
+// push(45)
+// includes()
+// Hinglish
+// BhaiLang
+// let arr1 = [21, 45, 32, 98]; 
+// console.log(arr1.includes(42));
