add7d5bae2e7799b3b4df3991ecb156e4db03505
Jatin3180 <jatinyadav31801@gmail.com>
2024-08-01 11:07:52 +0530

    function

--- Functions.js
+++ Functions.js
@@ -8,13 +8,34 @@
 // returned
 // caller
 // \
-let s1  = 2
-let s2 = 5
-let x = multiply(s1 , s2); //10
-let s3 = 10
-let s4 = 15
-let y = multiply(s3 , s4);
+// let s1  = 2
+// let s2 = 5
+// let x1 = multiply(s1 , s2); //
+// let s3 = 10
+// let s4 = 15
+// let y1 = multiply(s3 , s4);
 
-function multiply(a, b){
-    return a * b;
-}
+// function multiply(a, b){
+//     return a * b;
+// }
+// celsius = 5/9 * (frahenheit-32);
+// let frahenheit = 50;
+// let cel = convert(frahenheit)  //10
+// document.writeln(cel)
+
+// function convert(frahenheit){
+      
+//     celsius = 5/9 *(frahenheit-32);
+//     return -1;
+// }
+
+
+// let x = 10
+// let y =5
+
+// function swap(x , y){
+      
+// }
+
+
+
