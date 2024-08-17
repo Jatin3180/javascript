660829ac54406a383c718109f7120dbb2e04ff3e
Jatin3180 <jatinyadav31801@gmail.com>
2024-08-17 11:14:54 +0530

    doubts

--- doubts.js
+++ doubts.js
@@ -1,28 +1,30 @@
-let arr = [24, 47, 55, 32 , 14, 24]
-arr[2]
-let large = 0;
-let seclarge = arr[0];  //
-for(let i = 0;i<=arr.length;i++){
-    if(arr[i]>large){
-        large = arr[i]
-    }
-    else if(arr[i] > seclarge && seclarge != large){
-        seclarge = arr[i]
-    }
-}
-document.writeln(large)
-document.writeln(seclarge)
 
-for(let i = 0;i<arr.length;i++){    //outer loop
-   for(let j =1;j<arr.length;j++){  //inner loop
-    if(arr[i] == arr[j]){
-       document.writeln("duplicate element of index at = " + i + "of element"  + arr[i])
-    }
-    else{
-        document.writeln("duplicate not found!!!")
-    }
-   }
-}
+// let arr = [24, 47, 55, 32 , 14, 24]
+
+
+// let large = 0;
+// let seclarge = arr[0];  //
+// for(let i = 0;i<=arr.length;i++){
+//     if(arr[i]>large){
+//         large = arr[i]
+//     }
+//     else if(arr[i] > seclarge && seclarge != large){
+//         seclarge = arr[i]
+//     }
+// }
+// document.writeln(large)
+// document.writeln(seclarge)
+
+// for(let i = 0;i<arr.length;i++){    //outer loop
+//    for(let j =1;j<arr.length;j++){  //inner loop
+//     if(arr[i] == arr[j]){
+//        document.writeln("duplicate element of index at = " + i + "of element"  + arr[i])
+//     }
+//     else{
+//         document.writeln("duplicate not found!!!")
+//     }
+//    }
+// }
 // dry run
 // i = 0 - j=1 
 // i = 0 - j=2 
@@ -42,3 +44,21 @@ for(let i = 0;i<arr.length;i++){    //outer loop
 // }
 
 // 1. even number = 2 se divisible hote hai   
+
+// let x = document.getElementById("xyz");
+
+
+// Hoisting in js
+
+// x = 5
+// console.log(x)
+// var x;
+
+
+let x = 20;
+let y = 10;
+let r = x + y; //30
+let sum = eval("x + y");
+console.log(sum);   //30
+
+// 
